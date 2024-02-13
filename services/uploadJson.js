990f3c00aa58e6a2e48a6882f0945f516b697b39
windsywinds <windsor.sam@gmail.com>
2024-02-13 16:05:10 +1300

    company name

--- services/uploadJson.js
+++ services/uploadJson.js
@@ -24,7 +24,7 @@ async function createStorageBucketIfMissing(storage, bucketName) {
     // Create filename using the current time, company name and task index
     const date = new Date();
     date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
-    const filename = `${date.toISOString().replace(/:/g, '_')}-${jobData.companyName.replace(/:/g, '_').replace(/\//g, '_')}-task${taskIndex}`;
+    const filename = `${date.toISOString().replace(/:/g, '_')}-${companyName}-task${taskIndex}`;
     
     // Upload JSON data for all jobs related to the URL given
     const jsonData = jobData.jobData.map((job, index) => ({
